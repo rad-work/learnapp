49c6c3b2bf2f99880d263772ed637ad8f7ffa17a
Зудилина Мария <mary.zudilina@mail.ru>
2021-04-22 23:56:43 +0300

    Add style and some new pages

--- learnapp/post/urls.py
+++ learnapp/post/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from .views import PostListView
+from .views import PostListView, index_page, sign_in, sign_up, themes, sections, paragraph_1
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),
+    path('main', index_page),
+    path('sign_in', sign_in, name='sign_in'),
+    path('sign_up', sign_up, name='sign_up'),
+    path('themes', themes, name='themes'),
+    path('sections', sections, name='sections'),
+    path('paragraph_1', paragraph_1, name='paragraph_1'),
 ]
--- learnapp/post/views.py
+++ learnapp/post/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView
-
+from django.shortcuts import render
 from .models import Post
 
 
@@ -7,3 +7,26 @@ class PostListView(ListView):
     model = Post
     template_name = 'index.html'
 
+
+def index_page(request):
+    return render(request, 'index.html')
+
+
+def sign_in(request):
+    return render(request, 'sign_in.html')
+
+
+def sign_up(request):
+    return render(request, 'sign_up.html')
+
+
+def themes(request):
+    return render(request, 'themes.html')
+
+
+def sections(request):
+    return render(request, 'sections.html')
+
+
+def paragraph_1(request):
+    return render(request, 'paragraph_1.html')
